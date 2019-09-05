a56ef64d4a4743de05e13d1bc13d4c80f30e96fa
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-09-05 17:43:47 -0300

    Adicionar token

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 from landing.views import AlunoViewSet
 
@@ -15,4 +16,5 @@ router.register('professores', ProfessorViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('auth-api/', obtain_auth_token),
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from landing.models import Aluno
 from landing.serializers import AlunoSerializer
@@ -8,4 +10,6 @@ from landing.serializers import AlunoSerializer
 
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
     serializer_class = AlunoSerializer
