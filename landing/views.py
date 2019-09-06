7248b8ae8dffe171db3f995f004cdcbe4508ab71
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-09-06 15:29:16 -0300

    Add search

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from landing.models import Aluno
@@ -9,6 +10,8 @@ from landing.serializers import AlunoSerializer
 
 
 class AlunoViewSet(viewsets.ModelViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['^nome', '=idade']
     queryset = Aluno.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = (TokenAuthentication,)
